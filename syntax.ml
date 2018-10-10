9870fec36e845d6453fb80ad653ccdaa98640758
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-10-10 09:36:57 +0900

    pretty-printing

--- syntax.ml
+++ syntax.ml
@@ -7,5 +7,15 @@ type exp =
 
 let rec string_of_exp = function
   | Var i -> i
-  | Fun (i, e) -> Printf.sprintf "\\%s.%s" i (string_of_exp e)
-  | App (e1, e2) -> Printf.sprintf "(%s %s)" (string_of_exp e1) (string_of_exp e2)
+  | Fun (i, e) -> (
+      match e with
+      | App _ -> Printf.sprintf "\\%s.(%s)" i (string_of_exp e)
+      | _ -> Printf.sprintf "\\%s.%s" i (string_of_exp e)
+  )
+  | App (e1, e2) -> (
+      match e1, e2 with
+      | Fun _, Var _ -> Printf.sprintf "(%s) %s" (string_of_exp e1) (string_of_exp e2)
+      | Fun _, Fun _ -> Printf.sprintf "(%s) (%s)" (string_of_exp e1) (string_of_exp e2)
+      | _, App _ -> Printf.sprintf "%s (%s)" (string_of_exp e1) (string_of_exp e2)
+      | _, _ -> Printf.sprintf "%s %s" (string_of_exp e1) (string_of_exp e2)
+  )
