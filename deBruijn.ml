012316df6aa6781b741de0b3f7635a6b63a6ae3b
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-10-10 13:40:08 +0900

    beta reduction on De Bruijn index

--- deBruijn.ml
+++ deBruijn.ml
@@ -25,9 +25,41 @@ let rec body env depth = function
       let idx = depth - (Env.lookup i env) in
       Var idx
   | Syntax.Fun (i, t) ->
-      let newenv = Env.extend i depth env in
+      let newenv = Env.extend i (depth + 1) env in
       Fun (body newenv (depth + 1) t)
   | Syntax.App (t1, t2) ->
       App (body env depth t1, body env depth t2)
 
-let convert term = body Env.empty 0 term
+let rec shift c i = function
+  | Var n -> Var (if n < c then n else n + i)
+  | Fun t -> Fun (shift (c + 1) i t)
+  | App (t1, t2) -> App (shift c i t1, shift c i t2)
+
+let lift = shift 0
+
+let rec subst t m t1 =
+  match t1 with
+  | Var n -> if n = m then t else Var n
+  | Fun t1 ->
+      let t' = lift 1 t in
+      Fun (subst t' (m + 1) t1)
+  | App (t1, t2) -> App (subst t m t1, subst t m t2)
+
+let rec beta = function
+  | App (Fun t1, t2) ->
+      let t2' = lift 1 t2 in
+      let t1' = subst t2' 0 t1 in
+      lift (-1) t1'
+  | App (t1, t2) -> App (beta t1, beta t2)
+  | Fun t -> Fun (beta t)
+  | t -> t
+
+let rec fix f t =
+  let old = t in
+  let t' = f t in
+  if t' = old then t'
+  else fix f t'
+
+let convert term =
+  body Env.empty 0 term
+  |> fix beta
