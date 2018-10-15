7337f72c99b44c7436b5f1de473229a6ffbaf58e
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-10-15 13:42:15 +0900

    no type inference mode

--- main.ml
+++ main.ml
@@ -1,13 +1,20 @@
 let verbose = ref false
 
+let no_type_inference = ref true
+
 let rec repl () =
   print_string "# ";
   flush stdout;
   try
     let term = Parser.main Lexer.main (Lexing.from_channel stdin) in
-    let (_, ty) = Typing.type_infer term in
-    let indexed_term = DeBruijn.convert term !verbose in
-    Printf.printf "%s : %s\n" (DeBruijn.string_of_term indexed_term) (Syntax.string_of_ty ty);
+    if !no_type_inference then
+      let indexed_term = DeBruijn.convert term !verbose in
+      Printf.printf "%s\n" (DeBruijn.string_of_term indexed_term);
+      repl ()
+    else
+      let (_, ty) = Typing.type_infer term in
+      let indexed_term = DeBruijn.convert term !verbose in
+      Printf.printf "%s : %s\n" (DeBruijn.string_of_term indexed_term) (Syntax.string_of_ty ty);
     repl ()
   with
     | Typing.Error s ->
@@ -18,7 +25,9 @@ let usage = "Usage: " ^ Sys.argv.(0) ^ " [-v]"
 
 let aspec = Arg.align [
   ("-v", Arg.Unit (fun () -> verbose := true),
-  Printf.sprintf "Verbose mode. (default: %b)" !verbose)
+  Printf.sprintf "Verbose mode. (default: %b)" !verbose);
+  ("--no-type-inference", Arg.Unit (fun () -> no_type_inference := true),
+  Printf.sprintf "Disable type inference. (default: %b)" !no_type_inference);
 ]
 
 let () =
